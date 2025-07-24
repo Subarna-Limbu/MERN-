a8a000d33abab4255e3f9161d180e1ff32d003cf
Subarna Limbu <prasantlim401@gmail.com>
2025-07-24 15:18:10 +0545

    Initial commit: add strategy files

---
+++ Javascript/day1.js
@@ -0,0 +1,29 @@
+// Variables and Constant 
+var animal = " Define all animal ";
+let dog ="Dog bite";
+const eat=" All animal eats food";
+console.log("Define animal and eat data types = "+ animal + eat);
+
+// Type Checking
+let a =10;
+let b ="10";
+console.log(a==b);
+console.log(a===b);
+
+
+// Simple Calculator
+function Calculator(a,b){
+     var add = a+b;
+     console.log("Add : "+ add);
+
+     var subtract = a-b;
+     console.log("Subtract: "+ subtract);
+
+     var multiply = a*b;
+     console.log("Multiply: "+ multiply);
+
+     var divide = a/b;
+     console.log("divide : "+ divide);
+}
+Calculator(2,3);
+
---
+++ Javascript/day2.js
@@ -0,0 +1,18 @@
+// Function Declaration
+function greet(name){
+    return `Hello, ${name}`;
+}
+console.log(greet('Subarna'));
+
+//Function Expression
+const greeet = function(name){
+    return `Hello, ${name}`;
+};
+
+console.log(greeet('Limbu'));
+
+//Arrow Function
+const great = (names)=> `Hello,${names}`;
+console.log(great('Yes Sirrrrr'));
+
+// 
