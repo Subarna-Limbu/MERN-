a8f6af08af9e985ccb2e8fbe0082fcdaf6802757
Subarna Limbu <prasantlim401@gmail.com>
2025-08-12 18:55:01 +0545

    Updated

---
+++ Javascript/Learning JS/day17.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>🏠 Home Page</h2>;
+}
+
+function About() {
+  return <h2>ℹ️ About Page</h2>;
+}
+
+export default function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
