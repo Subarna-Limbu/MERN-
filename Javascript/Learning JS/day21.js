ee3e46ae4100507d3b30cab60f436e425316f306
Subarna Limbu <prasantlim401@gmail.com>
2025-08-16 22:19:03 +0545

    Updated

---
+++ Javascript/Learning JS/day21.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSy....",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
+
+export { signInWithPopup, signOut };
